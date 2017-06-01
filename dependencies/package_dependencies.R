a2217ff5c97b601b92954aef76805881b1f6c7c1
Stuart Illson <sillson88@gmail.com>
2017-06-01 15:29:41 -0700

    init empty R app

---
+++ dependencies/package_dependencies.R
@@ -0,0 +1,16 @@
+########################################
+# FireScout
+########################################
+firescout <-  c('dplyr',
+              'tidyr',
+              'rpart',
+              'rpart.plot',
+              'stringr',
+              'RPostgreSQL')
+
+########################################
+# Shiny App Packages
+########################################
+shiny_app_packages <- c('shiny',
+                        'ggplot2',
+                        'rAmCharts')
---
+++ firescout.R
@@ -0,0 +1,15 @@
+#################################
+## FireScout ##
+#################################
+
+##########
+## Set ENV
+##########
+source("~/firescout/dependencies/env_scripts.R",local=TRUE)
+loadEnv("firescout")
+
+############
+## Clean ENV
+############
+source("~/firescout/dependencies/env_scripts.R",local=TRUE)
+cleanEnv()
